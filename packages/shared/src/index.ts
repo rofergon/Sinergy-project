0e2983d7080b4bf567e4203e6dee1a9e3308fc60
sariclaw <sariclaw10@gmail.com>
2026-04-13 18:47:26 -0500

    feat: re-export strategyEngine from index.ts for improved module accessibility

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ export * from "./abis";
 export * from "./chain";
 export * from "./publicHosts";
 export * from "./strategy";
+export * from "./strategyEngine";
 export * from "./strategyTools";
 export * from "./zk";
