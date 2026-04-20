6b6d780c38ed93f8deb540c59ac725fe8b6e076f
sariclaw <sariclaw10@gmail.com>
2026-04-20 10:45:59 -0500

    feat: export strategyExecution module in index file

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from "./abis";
 export * from "./chain";
 export * from "./publicHosts";
 export * from "./strategy";
+export * from "./strategyExecution";
 export * from "./strategyEngine";
 export * from "./strategyTools";
 export * from "./zk";
